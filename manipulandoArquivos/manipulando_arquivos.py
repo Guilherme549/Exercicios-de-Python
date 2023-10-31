d4bded8486bdbe966036c9030523cc3c778be915
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2023-10-31 11:32:58 -0300

    .

---
+++ manipulandoArquivos/manipulando_arquivos.py
@@ -0,0 +1,12 @@
+# w: sobrescreve o texto no arquivo txt
+# a adiciona texto ao arquivo que ja tem texto 
+
+
+
+
+
+arquivo = open("manipulandoArquivos/Pessoa.txt", "w")
+
+arquivo.write("Oi ")
+
+ 
